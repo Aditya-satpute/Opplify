676290b8e892b492bb171900f08ae73fd7c66510
unknown <adityasatpute402@gmail.com>
2025-07-05 00:17:24 +0530

    Initialize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import { AppRoutes } from './routes/routes';
+
+function App() {
+  return (
+    <Router>
+      {/* <AppRoutes /> */}
+      <p>I am Opplify</p>
+    </Router>
+  );
+}
+
+export default App;
